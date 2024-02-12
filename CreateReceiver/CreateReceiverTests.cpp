f7780e216e4960b1bd15ef65f1a302264a5e95d4
Prakhar <prakharbhatt09@gmail.com>
2024-02-12 12:20:58 +0100

    updated 4 bit fields

--- CreateReceiver/CreateReceiverTests.cpp
+++ CreateReceiver/CreateReceiverTests.cpp
@@ -9,18 +9,18 @@ TEST_CASE("encode a message")
   CreateReceiver receiver1;
     receiver1.Cookie = 0x01;
     receiver1.PHY_ID = 1;
-    receiver1.CTRL_ID = 0;
+    receiver1.CTRL_ID = 2;
     receiver1.BroadcastStandard = 1;
-    receiver1.ReceiverMode = 0;
+    receiver1.ReceiverMode = 2;
     receiver1.Rfa = 0x0000;
 
     auto encoded_msg = receiver1.encode();
  
     CHECK(encoded_msg.at(0) == 0x01);
-    CHECK(encoded_msg.at(1) == 0x10);
-    CHECK(encoded_msg.at(2) == 0x00);
-    CHECK(encoded_msg.at(3) == 0x10);
-    CHECK(encoded_msg.at(4) == 0x00);
+    CHECK(encoded_msg.at(1) == 0x02);
+    CHECK(encoded_msg.at(2) == 0x03);
+    CHECK(encoded_msg.at(3) == 0x12);
+    CHECK(encoded_msg.at(4) == 0x12);
     CHECK(encoded_msg.at(5) == 0x00);
     CHECK(encoded_msg.at(6) == 0x00);
 
@@ -29,14 +29,16 @@ TEST_CASE("encode a message")
 TEST_CASE("decode a message")
 {
   CreateReceiver receiver2;
-  vector<uint8_t> toDecode{0x01,0x10,0x00,0x10,0x0000};
+  vector<uint8_t> toDecode{0x01,0x02,0x03,0x10,0x10,0x00,0x00};
 
   receiver2.decode(toDecode);
 
+    CHECK(receiver2.Cookie == 1);
+    //CHECK(receiver2.Opcode == 0x0203); macht sinn?
     CHECK(receiver2.PHY_ID == 1);
     CHECK(receiver2.CTRL_ID == 0);
     CHECK(receiver2.BroadcastStandard == 1);
     CHECK(receiver2.ReceiverMode == 0);
-    CHECK(receiver2.Rfa == 0x0000);
-
+    CHECK(receiver2.Rfa == 0x00);
+    
 }
