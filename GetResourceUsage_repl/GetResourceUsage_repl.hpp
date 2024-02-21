abe2201f0bf09392f9f6d130f5d78150f72694bb
Prakhar <prakharbhatt09@gmail.com>
2024-02-21 10:55:59 +0100

    hpp updated

--- GetResourceUsage_repl/GetResourceUsage_repl.hpp
+++ GetResourceUsage_repl/GetResourceUsage_repl.hpp
@@ -11,84 +11,125 @@ class GetResourceUsage_repl{
     const uint16_t Opcode{0x0203};
     uint8_t ReplyStatus;
     uint8_t Rfa;
-    uint8_t NrOfSoCs; //4bitmsb
-    vector<uint8_t> Rfa1;
-    vector<uint8_t> SoC_ID; //4bitmsb
-    vector<uint8_t> NrOfTunerPaths;
-    vector<uint8_t> AntennaHandle;
-    vector<uint8_t> Active;
-    vector<uint8_t> Rfa2;
-    vector<uint8_t> ActiveNBTs; //4bitmsb
-    vector<uint8_t> AntennaPort; //4bitmsb
-    vector<uint8_t> Rfa3;
-    vector<uint8_t> NBT_ix;
-    vector<uint8_t> Assigned_DFE_ix;
-    vector<uint8_t> Rfa4;
-    uint8_t NrOfDFEs;//
-    vector<uint8_t> NrOfAssignedReceivers;
-    vector<uint8_t> ReceiverHandle;
-    vector<uint8_t> BroadcastStandard; //4bitmsb
-    vector<uint8_t> Mode; //4bitmsb
-    vector<uint16_t> Rfa5;
-    vector<uint32_t> Rfa6;
-    vector<uint32_t> Rfa7;
+    uint8_t NrOfSoCs;
+    uint8_t Rfa7;
 
     public:
-    
     GetResourceUsage_repl();
 
     vector<uint8_t> encode();
     void decode(const vector<uint8_t>& toDecode);
-
+    
     uint8_t getCookie(){return Cookie;}
     uint8_t getReplyStatus(){return ReplyStatus;}
     uint8_t getRfa(){return Rfa;}
     uint8_t getNrOfSoCs(){return NrOfSoCs;}//4bitmsb
-    vector<uint8_t> getRfa1(){return Rfa1;}
-    vector<uint8_t> getSoC_ID(){return SoC_ID;}//4bitmsb
-    vector<uint8_t> getNrOfTunerPaths(){return NrOfTunerPaths;}
-    vector<uint8_t> getAntennaHandle(){return AntennaHandle;}
-    vector<uint8_t> getActive(){return Active;}
-    vector<uint8_t> getRfa2(){return Rfa2;}
-    vector<uint8_t> getActiveNBTs(){return ActiveNBTs;}//4bitmsb
-    vector<uint8_t> getAntennaPort(){return AntennaPort;}//4bitmsb
-    vector<uint8_t> getRfa3(){return Rfa3;}
-    vector<uint8_t> getNBT_ix(){return NBT_ix;}
-    vector<uint8_t> getAssigned_DFE_ix(){return Assigned_DFE_ix;}
-    vector<uint8_t> getRfa4(){return Rfa4;}
-    uint8_t getNrOfDFEs(){return NrOfDFEs;}
-    vector<uint8_t> getNrOfAssignedReceivers(){return NrOfAssignedReceivers;}
-    vector<uint8_t> getReceiverHandle(){return ReceiverHandle;}
-    vector<uint8_t> getBroadcastStandard(){return BroadcastStandard;}//4bitmsb
-    vector<uint8_t> getMode(){return Mode;}//4bitmsb
-    vector<uint16_t> getRfa5(){return Rfa5;}
-    vector<uint32_t> getRfa6(){return Rfa6;}
-    vector<uint32_t> getRfa7(){return Rfa7;}
+    uint32_t getRfa7(){return Rfa7;}
 
     void setCookie(uint8_t a){Cookie = a;}
     void setReplyStatus(uint8_t b){ReplyStatus = b;}
     void setRfa(uint8_t c){Rfa = c;}
     void setNrOfSoCs(uint8_t d){NrOfSoCs = d;}
-    void setRfa1(uint8_t e){Rfa1.push_back(e);}
-    void setSoC_ID(uint8_t f){SoC_ID.push_back(f);}
-    void setNrOfTunerPaths(uint8_t g){NrOfTunerPaths.push_back(g);}
-    void setAntennaHandle(uint8_t h){AntennaHandle.push_back(h);}
-    void setActive(uint8_t i){Active.push_back(i);}
-    void setRfa2(uint8_t j){Rfa2.push_back(j);}
+    void setRfa7(uint32_t z){Rfa7 = z;}
 
-    void setActiveNBTs(uint8_t k){ActiveNBTs.push_back(k);}//4bitmsb
-    void setAntennaPort(uint8_t m){AntennaPort.push_back(m);}//4bitmsb
-    void setRfa3(uint8_t n){Rfa3.push_back(n);}
-    void setNBT_ix(uint8_t p){NBT_ix.push_back(p);}
-    void setAssigned_DFE_ix(uint8_t q){Assigned_DFE_ix.push_back(q);}
-    void setRfa4(uint8_t r){Rfa4.push_back(r);}
-    void setNrOfDFEs(uint8_t s){NrOfDFEs = s;}
-    void setNrOfAssignedReceivers(uint8_t t){NrOfAssignedReceivers.push_back(t);}
-    void setReceiverHandle(uint8_t u){ReceiverHandle.push_back(u);}
-    void setBroadcastStandard(uint8_t v){BroadcastStandard.push_back(v);}//4bitmsb
-    void setMode(uint8_t w){Mode.push_back(w);}//4bitmsb
-    void setRfa5(uint8_t x){Rfa5.push_back(x);}
-    void setRfa6(uint8_t y){Rfa6.push_back(y);}
-    void setRfa7(uint32_t z){Rfa7.push_back(z);}
+        class SoC{
+        private:
+        uint8_t Rfa1;
+        uint8_t SoC_ID; //4bitmsb
+        uint8_t NrOfTunerPaths; //Max8
+        uint32_t Rfa6;
+        public:
+        SoC();
+
+        uint8_t getRfa1(){return Rfa1;}
+        uint8_t getSoC_ID(){return SoC_ID;}//4bitmsb
+        uint8_t getNrOfTunerPaths(){return NrOfTunerPaths;}
+        uint32_t getRfa6(){return Rfa6;}
+
+        void setRfa1(uint8_t e){Rfa1 = e;}
+        void setSoC_ID(uint8_t f){SoC_ID = f;}
+        void setNrOfTunerPaths(uint8_t g){NrOfTunerPaths = g;}
+        void setRfa6(uint8_t y){Rfa6 = y;}
+
+            class TunerPath{
+            private:
+            uint8_t AntennaHandle;
+            uint8_t Active;
+            uint8_t Rfa2;
+            uint8_t ActiveNBTs;
+            uint8_t AntennaPort;
+            uint8_t Rfa3;
+
+            public:
+            TunerPath();
+
+            uint8_t getAntennaHandle(){return AntennaHandle;}
+            uint8_t getActive(){return Active;}
+            uint8_t getRfa2(){return Rfa2;}
+            uint8_t getActiveNBTs(){return ActiveNBTs;}//4bitmsb
+            uint8_t getAntennaPort(){return AntennaPort;}//4bitmsb
+            uint8_t getRfa3(){return Rfa3;}
+
+            void setAntennaHandle(uint8_t h){AntennaHandle = h;}
+            void setActive(uint8_t i){Active = i;}
+            void setRfa2(uint8_t j){Rfa2 = j;}
+            void setActiveNBTs(uint8_t k){ActiveNBTs = k;}//4bitmsb
+            void setAntennaPort(uint8_t m){AntennaPort = m;}//4bitmsb
+            void setRfa3(uint8_t n){Rfa3 = n;}    
+        
+                class NBT{
+                private:
+                uint8_t NBT_ix;
+                uint8_t Assigned_DFE_ix;
+                uint8_t Rfa4;
+
+                public:
+                NBT();
+
+                uint8_t getNBT_ix(){return NBT_ix;}
+                uint8_t getAssigned_DFE_ix(){return Assigned_DFE_ix;}
+                uint8_t getRfa4(){return Rfa4;}
+
+                void setNBT_ix(uint8_t p){NBT_ix = p;}
+                void setAssigned_DFE_ix(uint8_t q){Assigned_DFE_ix = q;}
+                void setRfa4(uint8_t r){Rfa4 = r;}
+
+            };
+        };
+            class DFE{
+              private:
+                uint8_t NrOfDFEs;//Max 8
+
+              public:  
+                DFE();
+
+                uint8_t getNrOfDFEs(){return NrOfDFEs;}
+                void setNrOfDFEs(uint8_t s){NrOfDFEs = s;}
+
+                class Receiver{
+                    private:
+                    uint8_t NrOfAssignedReceivers;//Max 8
+                    uint8_t ReceiverHandle;
+                    uint8_t BroadcastStandard; //4bitmsb
+                    uint8_t Mode; //4bitmsb
+                    uint16_t Rfa5;
+
+                    public:
+                    Receiver();
+                    uint8_t getNrOfAssignedReceivers(){return NrOfAssignedReceivers;}
+                    uint8_t getReceiverHandle(){return ReceiverHandle;}
+                    uint8_t getBroadcastStandard(){return BroadcastStandard;}//4bitmsb
+                    uint8_t getMode(){return Mode;}//4bitmsb
+                    uint16_t getRfa5(){return Rfa5;}
+
+                    void setNrOfAssignedReceivers(uint8_t t){NrOfAssignedReceivers = t;}
+                    void setReceiverHandle(uint8_t u){ReceiverHandle = u;}
+                    void setBroadcastStandard(uint8_t v){BroadcastStandard = v;}//4bitmsb
+                    void setMode(uint8_t w){Mode = w;}//4bitmsb
+                    void setRfa5(uint8_t x){Rfa5 = x;}
+                };
+
+            };
+    };
 
 };
